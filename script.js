e7f16872de81fe6cde1182becc1b0d6ad4e1080e
Ron Sedee <roncd@hotmail.com>
2024-03-08 15:00:30 +0100

    dynamic favicon

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const colorscheme = document.querySelector('meta[name="color-scheme"]');
 const light = document.getElementById("light");
 const dark = document.getElementById("dark");
+const favicon = document.getElementById("favicon");
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     colorscheme.setAttribute("content", "dark");
     dark.style.display = "none";
+    favicon.href = "images/icon_dark.svg";
 } else {
     colorscheme.setAttribute("content", "light");
     light.style.display = "none";
@@ -15,6 +17,7 @@ document.getElementById("lightdark").addEventListener("click", () => {
         if ((window.getComputedStyle(col1).getPropertyValue("background-color")) == ("rgb(47, 79, 79)")) {
             col1.style.backgroundColor = "darkseagreen";
         }
+        favicon.href = "images/icon_light.svg";
         light.style.display = "none";
         dark.style.display = "initial";
     } else {
@@ -22,6 +25,7 @@ document.getElementById("lightdark").addEventListener("click", () => {
         if ((window.getComputedStyle(col1).getPropertyValue("background-color")) == ("rgb(143, 188, 143)")) {
             col1.style.backgroundColor = "darkslategrey";
         }
+        favicon.href = "images/icon_dark.svg";
         dark.style.display = "none";
         light.style.display = "initial";
     }
