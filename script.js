d4294f87758ed2739fec118a68707e3a9b63015c
Ron Sedee <roncd@hotmail.com>
2024-04-07 15:57:59 +0200

    Major styling update

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const dark = document.getElementById("dark");
 const favicon = document.getElementById("favicon");
 const col1 = document.getElementById("col-1");
 const allDetails = document.querySelectorAll("details");
+const summary = document.querySelectorAll("summary");
 const openp = document.querySelectorAll('details[open]');
 let printSchemeFlip;
 // Detect preferred dark/light scheme
@@ -11,25 +12,45 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
     colorscheme.setAttribute("content", "dark");
     dark.style.display = "none";
     favicon.href = "images/icon_dark.svg";
+    document.querySelector("html").className = ("dark");
+    document.querySelector("h1").className = ("dark");
     col1.className = "dark";
+    allDetails.forEach((detail) => {
+        detail.className = ("dark");
+    });
+    summary.forEach((summary) => {
+        summary.className = ("dark");
+    });
 } else {
     colorscheme.setAttribute("content", "light");
     light.style.display = "none";
+    document.querySelector("html").className = ("light");
+    document.querySelector("h1").className = ("light");
     col1.className = "light";
+    allDetails.forEach((detail) => {
+        detail.className = ("light");
+    });
+    summary.forEach((summary) => {
+        summary.className = ("dark");
+    });
 };
 // Dark/light scheme switch
 document.getElementById("lightdark").addEventListener("click", () => {
     if (colorscheme.getAttribute("content") == "dark") {
         colorscheme.setAttribute("content", "light");
-        col1.classList.remove("dark");
-        col1.classList.add("light");
+        document.querySelectorAll(".dark").forEach((el) => {
+            el.classList.add("light");
+            el.classList.remove("dark");
+        });
         favicon.href = "images/icon_light.svg";
         light.style.display = "none";
         dark.style.display = "initial";
     } else {
         colorscheme.setAttribute("content", "dark");
-        col1.classList.remove("light");
-        col1.classList.add("dark");
+        document.querySelectorAll(".light").forEach((el) => {
+            el.classList.add("dark");
+            el.classList.remove("light");
+        });
         favicon.href = "images/icon_dark.svg";
         dark.style.display = "none";
         light.style.display = "initial";
@@ -44,19 +65,19 @@ document.getElementById("print").addEventListener("click", () => {
         if (colorscheme.getAttribute("content") == "dark") {
             colorscheme.setAttribute("content", "light");
             printSchemeFlip = true;
-        }    
-    })
+        }
+    });
     window.addEventListener("afterprint", () => {
         if (printSchemeFlip) {
             colorscheme.setAttribute("content", "dark");
         }
         allDetails.forEach((detail) => {
             detail.open = false;
-        })
+        });
         openp.forEach((detail) => {
             detail.open = true;
-        })
-        printSchemeFlip = false; 
-    })
-    print();          
-})
+        });
+        printSchemeFlip = false;
+    });
+    print();
+});
